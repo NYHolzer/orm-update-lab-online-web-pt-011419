7fac68484253b65aec8e093d185ab1b7ab8d6b54
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-04 00:35:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,8 +63,12 @@ class Student
     new_student = self.new(row[0], row[1], row[2])
   end 
   
-  def self.find_by_name 
+  def self.find_by_name (name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
     
+    DB[:conn].execute(sql, name)
   end 
 end
 
